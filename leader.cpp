fda576a6b1076154c6a277d8af1b2af4d8c1e51c
Debu07 <mehtavedant7498@gmail.com>
2020-07-21 13:15:24 +0100

    find leaders in the array

---
+++ leader.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int findLeaders(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int j;
+        for(j=i+1;j<n;j++){
+            if(arr[i]<=arr[j])
+            break;
+        }
+        if(j==n)
+        cout<<arr[i]<<" ";
+    }
+}
+int main()
+{
+    int arr[]={16,17,4,3,5,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    findLeaders(arr,n);    
+    return 0;
+}
